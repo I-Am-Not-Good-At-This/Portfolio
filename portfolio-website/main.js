19c616d9c26afa297456b148ce59e941bb5233e6
I-Am-Not-Good-At-This <rhamaislaissezmoi@gmail.com>
2025-02-25 12:08:28 +0100

    start

---
+++ portfolio-website/main.js
@@ -0,0 +1,7 @@
+import './style.css'
+import './reset.css'
+import javascriptLogo from './javascript.svg'
+import viteLogo from '/vite.svg'
+
+
+const scottishSection = document.querySelector('.scottish-grid');
