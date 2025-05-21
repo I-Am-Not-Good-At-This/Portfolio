b53a3a3de551fbe61b4372fe18a0ec2f5962f655
I-Am-Not-Good-At-This <rhamaislaissezmoi@gmail.com>
2025-05-22 00:07:56 +0200

    disco time!

--- portfolio-website/main.js
+++ portfolio-website/main.js
@@ -1,7 +1,10 @@
-import './style.css'
-import './reset.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-
-
-const scottishSection = document.querySelector('.scottish-grid');
+function toggleDisco(button, target) {
+    const disco_shit = button.closest(target);
+    if (disco_shit.classList.contains('js-paused')) {
+        disco_shit.classList.remove('js-paused');
+        button.textContent = 'Arrêter les effets';
+    } else {
+        disco_shit.classList.add('js-paused');
+        button.textContent = 'Jouer les effets';
+    }
+}
